16c7b36546709864c75ce5afdceb6922f54e01b5
tommylees112 <thomas.lees112@gmail.com>
2021-02-16 17:29:10 +0000

    move run into functions

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ import pickle
 import xarray as xr
 from pathlib import Path
 from functools import partial
-from typing import Dict, Tuple
+from typing import Dict, Tuple, Optional
 from tqdm import tqdm
 
 # pytorch imports
@@ -20,58 +20,48 @@ from spatio_temporal.config import Config
 from spatio_temporal.training.trainer import Trainer
 
 
-if __name__ == "__main__":
-    #  TODO: move as much of this as possible to the Trainer object!
-    data_dir = Path("data")
-    run_dir = Path("runs")
+def _get_weight_file(cfg: Config, epoch: Optional[int] = None):
+    """Get file path to weight file"""
+    if epoch is None:
+        weight_file = sorted(list(cfg.run_dir.glob("model_epoch*.pt")))[-1]
+    else:
+        weight_file = cfg.run_dir / f"model_epoch{str(epoch).zfill(3)}.pt"
 
-    ds = pickle.load((data_dir / "kenya.pkl").open("rb"))
-    cfg = Config(cfg_path=Path("configs/config.yml"))
-    cfg.run_dir = run_dir
-    trainer = Trainer(cfg)
+    return weight_file
 
-    #  Settings
-    seed = cfg.seed
-    torch.manual_seed(seed)
-    np.random.seed(seed)
 
-    # Train test split
-    train_ds = ds[cfg.input_variables + [cfg.target_variable]].sel(
-        time=slice(cfg.train_start_date, cfg.train_end_date)
-    )
-    valid_ds = ds[cfg.input_variables + [cfg.target_variable]].sel(
-        time=slice(cfg.validation_start_date, cfg.validation_end_date)
-    )
-    test_ds = ds[cfg.input_variables + [cfg.target_variable]].sel(
-        time=slice(cfg.test_start_date, cfg.test_end_date)
-    )
-
-    # Get DataLoaders
-    base_dl = partial(
-        PixelDataLoader,
-        **dict(
-            cfg=cfg,
-            target_variable=cfg.target_variable,
-            input_variables=cfg.input_variables,
-            pixel_dims=cfg.pixel_dims,
-            num_workers=cfg.num_workers,
-            seq_length=cfg.seq_length,
-            batch_size=cfg.batch_size,
-            autoregressive=cfg.autoregressive,
-        ),
-    )
-
-    train_dl = base_dl(train_ds, mode="train")
-    valid_dl = base_dl(valid_ds, mode="validation")
-    test_dl = base_dl(test_ds, mode="test")
-    normalizer = train_dl.normalizer
-
+def run_test(cfg: Config, test_dl: PixelDataLoader):
+    # RUN TEST !
     model = LSTM(
-        input_size=train_dl.input_size,
+        input_size=test_dl.input_size,
         hidden_size=cfg.hidden_size,
-        output_size=train_dl.output_size,
+        output_size=test_dl.output_size,
     )
 
+    weight_file = _get_weight_file(cfg)
+    model.load_state_dict(torch.load(weight_file, map_location=cfg.device))
+    
+    model.eval()
+    with torch.no_grad():
+        test_pbar = tqdm(test_dl, desc=f"Test set Forward Pass: ")
+        for x, y in test_pbar:
+            y_hat = model(x)
+            # back convert to xarray object ...
+            assert False
+            
+            # rescale predictions
+            # test_dl.normalizer.individual_inverse(data, pixel_id=pixel_id, variable=cfg.target_variable)
+
+            # create xarray object (sim, obs)
+            # _create_xarray(y_hat, y)
+
+            # save results
+            # _save_results(results, epoch)
+
+
+def train(cfg, train_dl, valid_dl, model):
+    # TODO: move as much of this as possible to the Trainer object!
+    # get loss & optimizer
     if cfg.loss == "MSE":
         loss_fn = nn.MSELoss()
 
@@ -80,7 +70,6 @@ if __name__ == "__main__":
             [pam for pam in model.parameters()], lr=cfg.learning_rate
         )
 
-    assert False
     for epoch in range(cfg.n_epochs):
         train_loss = []
         #  batch the training data
@@ -89,7 +78,7 @@ if __name__ == "__main__":
             # forward pass
             y_hat = model(x)
 
-            # measure loss
+            # measure loss on all (test memory) or only on last (test_forecast)
             loss = loss_fn(y_hat["y_hat"], y)
 
             # backward pass
@@ -121,4 +110,54 @@ if __name__ == "__main__":
         optimizer_path = cfg.run_dir / f"optimizer_state_epoch{epoch:03d}.pt"
         torch.save(optimizer.state_dict(), str(optimizer_path))
 
-    #  RUN TEST !
+
+if __name__ == "__main__":
+    TRAIN = False
+    data_dir = Path("data")
+    run_dir = Path("runs")
+
+    ds = pickle.load((data_dir / "kenya.pkl").open("rb"))
+
+    if TRAIN:
+        cfg = Config(cfg_path=Path("configs/config.yml"))
+        cfg.run_dir = run_dir
+        trainer = Trainer(cfg, ds)
+
+        # Train test split
+        train_ds = ds[cfg.input_variables + [cfg.target_variable]].sel(
+            time=slice(cfg.train_start_date, cfg.train_end_date)
+        )
+        valid_ds = ds[cfg.input_variables + [cfg.target_variable]].sel(
+            time=slice(cfg.validation_start_date, cfg.validation_end_date)
+        )
+        
+        # Get DataLoaders
+        dl = train_dl = PixelDataLoader(train_ds, cfg=cfg, mode="train")
+        valid_dl = PixelDataLoader(valid_ds, cfg=cfg, mode="validation")
+
+    else:
+        cfg = Config(cfg_path=Path("runs/test_1602_154323/config.yml"))
+
+        test_ds = ds[cfg.input_variables + [cfg.target_variable]].sel(
+            time=slice(cfg.test_start_date, cfg.test_end_date)
+        )
+
+        # Get DataLoaders
+        dl = test_dl = PixelDataLoader(test_ds, cfg=cfg, mode="test")
+    
+    # Settings
+    seed = cfg.seed
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+
+    model = LSTM(
+        input_size=dl.input_size,
+        hidden_size=cfg.hidden_size,
+        output_size=dl.output_size,
+    )
+
+    if TRAIN:
+        train(cfg, train_dl, valid_dl, model)
+    else:
+        run_test(cfg, test_dl)
+
