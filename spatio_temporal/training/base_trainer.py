8ca6411628b390e14eb594b409f2c4cc7b63f8ee
tommylees112 <thomas.lees112@gmail.com>
2021-02-18 17:02:32 +0000

    start Trainer translation (ONGOING)

--- spatio_temporal/training/base_trainer.py
+++ spatio_temporal/training/base_trainer.py
@@ -1,3 +1,8 @@
+from datetime import datetime
+from typing import Optional
+import random
+from pprint import pprint
+import numpy as np
 from pathlib import Path
 import torch
 from spatio_temporal.config import Config
@@ -5,4 +10,73 @@ from spatio_temporal.config import Config
 
 class BaseTrainer:
     def __init__(self, cfg: Config):
-        pass
+        self.cfg = cfg
+        self.model = None
+        self.optimizer = None
+
+    def _set_random_seeds(self):
+        if self.cfg.seed is None:
+            self.cfg.seed = int(np.random.uniform(low=0, high=1e6))
+
+        # fix random seeds for various packages
+        random.seed(self.cfg.seed)
+        np.random.seed(self.cfg.seed)
+        torch.cuda.manual_seed(self.cfg.seed)
+        torch.manual_seed(self.cfg.seed)
+
+    def _create_folder_structure(self):
+        run_name = self._create_datetime_folder_name()
+        if self.cfg.run_dir is None:
+            self.cfg.run_dir = Path().cwd() / "runs" / run_name
+        else:
+            self.cfg.run_dir = self.cfg.run_dir / run_name
+
+        self.cfg.run_dir.mkdir(parents=True, exist_ok=True)
+
+    def _create_datetime_folder_name(self) -> str:
+        now = datetime.now()
+        day = f"{now.day}".zfill(2)
+        month = f"{now.month}".zfill(2)
+        hour = f"{now.hour}".zfill(2)
+        minute = f"{now.minute}".zfill(2)
+        second = f"{now.second}".zfill(2)
+        run_name = f"{self.cfg.experiment_name}_{day}{month}_{hour}{minute}{second}"
+        return run_name
+
+    def _save_weights_and_optimizer(self, epoch: int):
+        weight_path = self.cfg.run_dir / f"model_epoch{epoch:03d}.pt"
+        torch.save(self.model.state_dict(), str(weight_path))
+
+        optimizer_path = self.cfg.run_dir / f"optimizer_state_epoch{epoch:03d}.pt"
+        torch.save(self.optimizer.state_dict(), str(optimizer_path))
+
+    def _get_weight_file(self, epoch: Optional[int]):
+        """Get file path to weight file"""
+        if epoch is None:
+            weight_file = sorted(list(self.cfg.run_dir.glob("model_epoch*.pt")))[-1]
+        else:
+            weight_file = self.cfg.run_dir / f"model_epoch{str(epoch).zfill(3)}.pt"
+
+        return weight_file
+
+    def _load_weights(self, epoch: int = None):
+        """Load weights of a certain (or the last) epoch into the model."""
+        weight_file = self._get_weight_file(epoch)
+
+        self.model.load_state_dict(torch.load(weight_file, map_location=self.device))
+
+    def _set_device(self):
+        if self.cfg.device is not None:
+            if self.cfg.device.startswith("cuda"):
+                gpu_id = int(self.cfg.device.split(":")[-1])
+                if gpu_id > torch.cuda.device_count():
+                    raise RuntimeError(f"This machine does not have GPU #{gpu_id} ")
+                else:
+                    self.device = torch.device(self.cfg.device)
+            else:
+                self.device = torch.device("cpu")
+        else:
+            self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    def __repr__(self):
+        pprint(self.cfg._cfg)
--- spatio_temporal/training/trainer.py
+++ spatio_temporal/training/trainer.py
@@ -1,22 +1,18 @@
-from datetime import datetime
-from pathlib import Path
 import torch
-from spatio_temporal.config import Config
-import random
 import numpy as np
-from typing import Optional
 import xarray as xr
 
-# from spatio_temporal.training.base_trainer import BaseTrainer
+from spatio_temporal.training.base_trainer import BaseTrainer
+from spatio_temporal.data.dataloader import PixelDataLoader
+from spatio_temporal.config import Config
 
 
-class Trainer:
+class Trainer(BaseTrainer):
     def __init__(self, cfg: Config, ds: xr.Dataset):
-        self.cfg = cfg
+        super().__init__(cfg=cfg)
+
         # set / use the run directory
         self._create_folder_structure()
-        self.model = None
-        self.optimizer = None
         self.device = cfg.device
 
         #  set random seeds
@@ -25,93 +21,43 @@ class Trainer:
         self.cfg.dump_config(self.cfg.run_dir)
 
         #  initialise dataloaders
+        #  initialise model
         # train epochs
 
-    def initialise_data(ds, mode: str = "train"):
+    def initialise_data(self, ds, mode: str = "train") -> None:
+        """Load data from DataLoaders and store as attributes
+
+        Args:
+            ds ([type]): [description]
+            mode (str, optional): [description]. Defaults to "train".
+
+        Attributes:
+            if mode == "train"
+                self.train_dl ([PixelDataLoader]):
+                self.valid_dl ([PixelDataLoader]):
+            elif mode == "test"
+                self.test_dl ([PixelDataLoader]):
+        """
         # Train test split
         if mode == "train":
             # train period
-            train_ds = ds[cfg.input_variables + [cfg.target_variable]].sel(
-                time=slice(cfg.train_start_date, cfg.train_end_date)
+            train_ds = ds[self.cfg.input_variables + [self.cfg.target_variable]].sel(
+                time=slice(self.cfg.train_start_date, self.cfg.train_end_date)
             )
-            self.train_dl = PixelDataLoader(train_ds, cfg=cfg, mode="train")
+            self.train_dl = PixelDataLoader(train_ds, cfg=self.cfg, mode="train")
 
             #  validation period
-            valid_ds = ds[cfg.input_variables + [cfg.target_variable]].sel(
-                time=slice(cfg.validation_start_date, cfg.validation_end_date)
+            valid_ds = ds[self.cfg.input_variables + [self.cfg.target_variable]].sel(
+                time=slice(self.cfg.validation_start_date, self.cfg.validation_end_date)
             )
-            self.valid_dl = PixelDataLoader(valid_ds, cfg=cfg, mode="validation")
+            self.valid_dl = PixelDataLoader(valid_ds, cfg=self.cfg, mode="validation")
 
             self.normalizer = self.train_dl.normalizer
         else:
             # test period
-            test_ds = ds[cfg.input_variables + [cfg.target_variable]].sel(
-                time=slice(cfg.test_start_date, cfg.test_end_date)
+            test_ds = ds[self.cfg.input_variables + [self.cfg.target_variable]].sel(
+                time=slice(self.cfg.test_start_date, self.cfg.test_end_date)
             )
             self.test_dl = PixelDataLoader(
-                test_ds, cfg=cfg, mode="test", normalizer=self.normalizer
+                test_ds, cfg=self.cfg, mode="test", normalizer=self.normalizer
             )
-
-    def _set_random_seeds(self):
-        if self.cfg.seed is None:
-            self.cfg.seed = int(np.random.uniform(low=0, high=1e6))
-
-        # fix random seeds for various packages
-        random.seed(self.cfg.seed)
-        np.random.seed(self.cfg.seed)
-        torch.cuda.manual_seed(self.cfg.seed)
-        torch.manual_seed(self.cfg.seed)
-
-    def _create_folder_structure(self):
-        run_name = self._create_datetime_folder_name()
-        if self.cfg.run_dir is None:
-            self.cfg.run_dir = Path().cwd() / "runs" / run_name
-        else:
-            self.cfg.run_dir = self.cfg.run_dir / run_name
-
-        self.cfg.run_dir.mkdir(parents=True, exist_ok=True)
-
-    def _create_datetime_folder_name(self) -> str:
-        now = datetime.now()
-        day = f"{now.day}".zfill(2)
-        month = f"{now.month}".zfill(2)
-        hour = f"{now.hour}".zfill(2)
-        minute = f"{now.minute}".zfill(2)
-        second = f"{now.second}".zfill(2)
-        run_name = f"{self.cfg.experiment_name}_{day}{month}_{hour}{minute}{second}"
-        return run_name
-
-    def _save_weights_and_optimizer(self, epoch: int):
-        weight_path = self.cfg.run_dir / f"model_epoch{epoch:03d}.pt"
-        torch.save(self.model.state_dict(), str(weight_path))
-
-        optimizer_path = self.cfg.run_dir / f"optimizer_state_epoch{epoch:03d}.pt"
-        torch.save(self.optimizer.state_dict(), str(optimizer_path))
-
-    def _get_weight_file(self, epoch: Optional[int]):
-        """Get file path to weight file"""
-        if epoch is None:
-            weight_file = sorted(list(self.cfg.run_dir.glob("model_epoch*.pt")))[-1]
-        else:
-            weight_file = self.cfg.run_dir / f"model_epoch{str(epoch).zfill(3)}.pt"
-
-        return weight_file
-
-    def _load_weights(self, epoch: int = None):
-        """Load weights of a certain (or the last) epoch into the model."""
-        weight_file = self._get_weight_file(epoch)
-
-        self.model.load_state_dict(torch.load(weight_file, map_location=self.device))
-
-    def _set_device(self):
-        if self.cfg.device is not None:
-            if self.cfg.device.startswith("cuda"):
-                gpu_id = int(self.cfg.device.split(":")[-1])
-                if gpu_id > torch.cuda.device_count():
-                    raise RuntimeError(f"This machine does not have GPU #{gpu_id} ")
-                else:
-                    self.device = torch.device(self.cfg.device)
-            else:
-                self.device = torch.device("cpu")
-        else:
-            self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
