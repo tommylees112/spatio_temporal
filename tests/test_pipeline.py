eb715f0649791d44af914884dc728cc04e62d685
tommylees112 <thomas.lees112@gmail.com>
2021-03-19 13:12:02 +0000

    create test script for the linear example

---
+++ tests/test_pipeline.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+from spatio_temporal.config import Config
+from spatio_temporal.training.trainer import Trainer
+from spatio_temporal.training.tester import Tester
+from tests.utils import create_linear_ds
+from spatio_temporal.training.eval_utils import save_loss_curves, save_timeseries
+
+
+class TestLinear:
+    def test_linear_example(self):
+        ds = create_linear_ds(epsilon_sigma=10)
+        cfg = Config(Path("tests/testconfigs/test_config.yml"))
+
+        #  Train
+        trainer = Trainer(cfg, ds)
+        losses = trainer.train_and_validate()
+        save_loss_curves(losses, cfg)
+
+        # Test
+        tester = Tester(cfg, ds)
+        preds = tester.run_test()
+        for _ in range(2):
+            save_timeseries(preds, cfg)
+
+
+if __name__ == "__main__":
+    t = TestLinear()
+    t.test_linear_example()
